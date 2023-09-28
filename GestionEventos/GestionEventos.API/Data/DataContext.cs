da896261776809eb39c89b11dd6eb8818236133a
nicohernandezgg@gmail.com <nicohernandezgg@gmail.com>
2023-09-27 23:40:46 -0500

    Entidades, Controladores, NuGets

---
+++ GestionEventos/GestionEventos.API/Data/DataContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using GestionEventos.Shared.Entities;
+
+
+
+
+namespace GestionEventos.API.Data
+{
+    public class DataContext : DbContext
+    {
+
+       
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+
+
+        }
+
+        public DbSet<EventoAcademico> Eventos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Participantes> Participante { get; set; }
+
+        public DbSet<ProgramaEvento> Programa { get; set; }
+
+      
+    }
+
+
+}
---
+++ GestionEventos/GestionEventos.Shared/Entities/EventoAcademico.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestionEventos.Shared.Entities
+{
+    public class EventoAcademico
+    {
+
+        public int Id { get; set; }
+
+        [Display(Name = "Evento")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe contener únicamente 100 caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string? Name { get; set; }
+
+
+        [Display(Name = "Fecha de Inicio")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public DateTime FechaInicio { get; set; }
+
+        [Display(Name = "Fecha de Finalización")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public DateTime FechaFin { get; set; }
+
+        [Display(Name = "Ubicación")]
+        [MaxLength(150, ErrorMessage = "El campo {0} debe contener únicamente 150 caracteres")]
+        public string? Ubiacion { get; set; }
+
+        [Display(Name = "Descripción")]
+        public string? Description { get; set; }
+
+        [Display(Name = "Tema")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe contener únicamente 50 caracteres")]
+        public string? Tematica { get; set; }
+
+
+
+
+    }
+}
